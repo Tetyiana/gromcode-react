873ced126249acf7388633a8d7be7c7831b6b173
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-10-17 22:38:48 +0300

    try4

--- react-intro/react-app-jsx/src/index.js
+++ react-intro/react-app-jsx/src/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './styles.css';
 
 const rootElement = document.querySelector('#root');
 
-const root = createRoot(rootElement);
-
 const elem = (
-  <div className='greeting'>
+  <div className='greeting' >
     <h1 className='greeting__title'>Hello, world!</h1>
     <p className='greeting__text'>I am learning React</p>
   </div>
 );
 
-root.render(elem);
+
+ReactDOM.render(elem, rootElement);
+
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import './styles.css';
+
+// const rootElement = document.querySelector('#root');
+
+// const root = createRoot(rootElement);
+
+// const elem = (
+//   <div className='greeting'>
+//     <h1 className='greeting__title'>Hello, world!</h1>
+//     <p className='greeting__text'>I am learning React</p>
+//   </div>
+// );
+
+// root.render(elem);
