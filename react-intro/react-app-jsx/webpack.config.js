e93e986695352ae4f633975846d5801b167951d3
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-10-17 22:27:27 +0300

    try2

--- react-intro/react-app-jsx/webpack.config.js
+++ react-intro/react-app-jsx/webpack.config.js
@@ -1,30 +1,26 @@
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: 'bundle.js'
-  },
-  stats: {
-    children: true,
-  },
+  mode: 'development',
+
   module: {
     rules: [
       {
         test: /.js$/,
-        use: ['babel-loader',]
+        use: ['babel-loader']
       },
       {
         test: /.css$/,
         use: ['style-loader', 'css-loader',]
-      }
+      },
 
     ]
   },
-  // plugins: [
-  //   new HtmlWebpackPlugin({
-  //     template: './src/index.html'
-  //   })
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+  ],
 
-  // ]
 }
