2c3df3b5fb2a209cb39d78b8d278bd06a2039761
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-10-20 17:23:02 +0300

    try1

---
+++ components-props/comment-component/src/index.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Comment from './Comment.jsx';
+
+
+
+const rootElement = document.querySelector('#root');
+
+const userInfo = {
+  name: 'Tom',
+  avatarUrl: 'https://avatars3.githubusercontent.com/u10001'
+}
+
+ReactDOM.render(<Comment
+  user={userInfo}
+  text='Good job!'
+  date={new Date()}
+/>, rootElement);
