7cc49aa91bb286b9481177ff7126018697c20d66
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-10-24 14:55:08 +0300

    try1

---
+++ state/tipical-counter/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from "./Counter";
+const App = () => {
+  return (
+  <>
+      <Counter start={5} interval={1000} /> 
+      <Counter start={12} interval={100} />
+       <Counter start={-21} interval={6000 } />
+  </>
+   
+  );
+};
+
+export default App;
---
+++ state/tipical-counter/src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App  from "./App.jsx";
+
+
+const rootElement = document.querySelector('#root');
+
+
+
+// const element = <div>Hi</div>;
+
+ReactDOM.render(<App />, rootElement);
