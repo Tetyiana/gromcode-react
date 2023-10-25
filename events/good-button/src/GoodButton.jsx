bdffdb7c7dc957a4e8184f91f1858c4cf3f0a107
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-10-25 19:45:23 +0300

    try1

---
+++ events/good-button/src/GoodButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const GoodButton = () => {
+  return (
+    <button
+      class="fancy-button"
+      onClick={()=> alert('Good job!')}
+      
+    >
+      Click me!</button>
+)
+};
+
+export default GoodButton;
