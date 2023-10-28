8114cea99a4694118fab155f65692b9f15916f08
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-10-28 13:30:54 +0300

    try2

--- conditional-rendering/if-rendering/src/GuestGreeting.jsx
+++ conditional-rendering/if-rendering/src/GuestGreeting.jsx
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 const GuestGreeting = () => {
  
   return (
-    <h1>Hi Stranger &#9996;&#65039; Please login &#128640;</h1>
+    <h1>Hi Stranger. Please login &#128640;</h1>
   )
 }
 export default GuestGreeting;
