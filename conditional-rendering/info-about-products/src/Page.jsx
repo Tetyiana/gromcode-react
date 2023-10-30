e5321e2be0c3ce18f678e2a3cbee9ebda4fe2b27
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-10-30 12:26:08 +0200

    try27

--- conditional-rendering/info-about-products/src/Page.jsx
+++ conditional-rendering/info-about-products/src/Page.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import  Info  from "./Info.jsx";
 
 const text1 = 'Price is 500$. Available in 2 colors';
-const text2 = 'Price is 650$. Not available.';
+const text2 = 'Price is 650$. Not available';
 
 
 class Page extends Component {
