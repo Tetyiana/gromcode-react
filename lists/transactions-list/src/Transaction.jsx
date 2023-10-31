d7ef84341f5ff0390fc3fe184fd89ca4e000eb0c
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-10-31 12:51:49 +0200

    try7

--- lists/transactions-list/src/Transaction.jsx
+++ lists/transactions-list/src/Transaction.jsx
@@ -16,7 +16,7 @@ const Transaction = ({ transaction }) => {
       <span className="transaction__time">{formattedTime}</span>
       <span className="transaction__assets">{assets}</span>
       <span className="transaction__rate">{formattedRate}</span>
-      <span className="transaction__amount">{amount}</span>
+      <span className="transaction__amount">{new Intl.NumberFormat('en-GB').format(amount)}</span>
     </li>
   );
 }
