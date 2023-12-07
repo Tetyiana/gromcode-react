c91472c63b830ad691684364ab5f340da6ab1d9e
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-12-07 21:46:32 +0200

    try6

--- lifecycle-methods/dimensions/src/index.jsx
+++ lifecycle-methods/dimensions/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Dimension from './Dimension.jsx';
+import Dimension from './Dimensions.jsx';
 
 const rootElement = document.querySelector('#root');
 
