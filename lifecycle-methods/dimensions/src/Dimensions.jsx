af865583cdfa9095b314767ddd4498064860599d
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-12-07 21:38:46 +0200

    try1

---
+++ lifecycle-methods/dimensions/src/Dimensions.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class Dimensions extends Component {
+  state = {
+      width: null,
+      height: null,
+    }
+
+  componentDidMount() {
+    const { innerWidth, innerHeight } = window;
+    window.addEventListener('resize', this.onResize)
+    this.setDimensions(innerWidth, innerHeight);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize)
+  }
+ 
+  onResize = e => {
+    const { innerWidth, innerHeight } = e.target;
+    this.setDimensions(innerWidth, innerHeight);
+  }
+  setDimensions = (width, height) => {
+     this.setState({
+      width,
+      height,
+    })
+    document.title = `${innerWidth}x${innerHeight}`;
+  }
+  render() {
+    return (
+    <div className="dimensions">{`${this.state.width}px-${this.state.height}px`}</div>
+  )
+}
+}
+export default Dimensions;
