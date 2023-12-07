c66738ffde243929c3eba3b41807bac18114bcfd
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-12-07 19:26:27 +0200

    try8

--- lifecycle-methods/lifecycle/src/Life.jsx
+++ lifecycle-methods/lifecycle/src/Life.jsx
@@ -3,23 +3,23 @@ import React, { Component } from "react";
 class Life extends Component {
   constructor(props) {
     super(props);
-    console.log('constructor: good place to create state')
+    console.log('good place to create state')
   }
   componentDidMount() {
-    console.log('componentDidMount: API calls, subscriptions')
+    console.log('API calls, subscriptions')
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+    console.log('decide to render or not to render');
     return nextProps.number % 2;
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props')
+    console.log('some updates based on new props')
   }
 
     componentWillUnmount() {
-    console.log('componentWillUnmount: cleanup before DOM related to component will be removed')
+    console.log('cleanup before DOM related to component will be removed')
   }
 
   render() {
