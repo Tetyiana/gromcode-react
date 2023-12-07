62e53a2d7302e573bac50b59dbe1f1c57eefea9a
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-12-07 22:52:06 +0200

    try11

--- lifecycle-methods/lifecycle/src/Life.jsx
+++ lifecycle-methods/lifecycle/src/Life.jsx
@@ -3,31 +3,35 @@ import React, { Component } from "react";
 class Life extends Component {
   constructor(props) {
     super(props);
-    console.log('good place to create state')
+    console.log('good place to create state');
   }
+
   componentDidMount() {
-    console.log('API calls, subscriptions')
+    console.log('API calls, subscriptions');
   }
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log('decide to render or not to render');
-    return nextProps.number % 2;
+    return true;
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log('some updates based on new props')
+    console.log('some updates based on new props');
   }
 
-    componentWillUnmount() {
-    console.log('cleanup before DOM related to component will be removed')
+  componentWillUnmount() {
+    console.log('cleanup before DOM related to component will be removed');
   }
 
   render() {
-     console.log('return React element to build DOM');
+    console.log('return React element to build DOM');
+    
     return (
-          <div className="number">{this.props.number}</div>
-   )
+      <div className="life">
+     86
+      </div>
+    );
   }
-  
 }
+
 export default Life;
