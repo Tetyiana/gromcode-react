3631504afffeac4ef2838c88059cc99450c63fb2
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-12-07 19:15:31 +0200

    try6

--- lifecycle-methods/lifecycle/src/Life.jsx
+++ lifecycle-methods/lifecycle/src/Life.jsx
@@ -8,16 +8,20 @@ class Life extends Component {
   componentDidMount() {
     console.log('componentDidMount: API calls, subscriptions')
   }
+
   shouldComponentUpdate(nextProps, nextState) {
     console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
-    return (true);
+    return nextProps.number % 2;
   }
+
   componentDidUpdate(prevProps, prevState) {
     console.log('componentDidUpdate(prevProps, prevState): some updates based on new props')
   }
+
     componentWillUnmount() {
     console.log('componentWillUnmount: cleanup before DOM related to component will be removed')
   }
+  
   render() {
      console.log('return React element to build DOM');
     return (
