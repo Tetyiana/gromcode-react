b16f60761b5c7c86bcb57320832afd3d313f5f66
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-12-08 23:00:22 +0200

    екн18

--- lifecycle-methods/connection-status/src/ConnectionStatus.jsx
+++ lifecycle-methods/connection-status/src/ConnectionStatus.jsx
@@ -27,7 +27,7 @@ class ConnectionStatus extends Component {
   render() {
     return (
       <div className={this.state.isOnline ? 'status' : 'status status_offline'}>
-        {this.state.isOnline ? 'Online' : 'Offline'}
+        {this.state.isOnline ? 'online' : 'оffline'}
       </div>
     );
   }
