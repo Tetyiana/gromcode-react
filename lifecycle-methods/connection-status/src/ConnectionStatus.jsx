b34ca474ae6f3f4f4a40b9b1c13941495683ef45
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-12-08 18:00:34 +0200

    try1

---
+++ lifecycle-methods/connection-status/src/ConnectionStatus.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class ConnectionStatus extends Component {
+  state = {
+    online: true,
+  };
+
+  componentDidMount() {
+    window.addEventListener('online', this.updateOnlineStatus);
+    window.addEventListener('offline', this.updateOnlineStatus);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('online', this.updateOnlineStatus);
+    window.removeEventListener('offline', this.updateOnlineStatus);
+  }
+
+  updateOnlineStatus = () => {
+    this.setState({
+      online: navigator.onLine,
+    });
+  };
+
+  render() {
+    const statusClassName = this.state.online ? 'status status_online' : 'status status_offline';
+
+    return (
+      <div className={statusClassName}>
+        {this.state.online ? 'Online' : 'Offline'}
+      </div>
+    );
+  }
+}
+
+export default ConnectionStatus;
