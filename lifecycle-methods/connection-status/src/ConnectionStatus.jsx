232e2df143a34c5fcbf7ff7a77b5d01ec2cffdcf
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-12-09 11:55:16 +0200

    try19

--- lifecycle-methods/connection-status/src/ConnectionStatus.jsx
+++ lifecycle-methods/connection-status/src/ConnectionStatus.jsx
@@ -1,40 +1,74 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-class ConnectionStatus extends Component {
+// class ConnectionStatus extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isOnline: true,
+//     };
+//   }
+
+//   componentDidMount() {
+//     window.addEventListener('online', this.updateOnlineStatus);
+//     window.addEventListener('offline', this.updateOnlineStatus);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('online', this.updateOnlineStatus);
+//     window.removeEventListener('offline', this.updateOnlineStatus);
+//   }
+
+//   updateOnlineStatus = () => {
+//     this.setState({
+//       isOnline: navigator.onLine,
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div className={this.state.isOnline ? 'status' : 'status status_offline'}>
+//         {this.state.isOnline ? 'online' : 'оffline'}
+//       </div>
+//     );
+//   }
+// }
+// export default ConnectionStatus;
+
+import React, { Component } from 'react';
+
+class Status extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isOnline: true,
+      status: 'online',
     };
   }
 
   componentDidMount() {
-    window.addEventListener('online', this.updateOnlineStatus);
-    window.addEventListener('offline', this.updateOnlineStatus);
+    window.addEventListener('online', this.handleOnline);
+    window.addEventListener('offline', this.handleOffline);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('online', this.updateOnlineStatus);
-    window.removeEventListener('offline', this.updateOnlineStatus);
+    window.removeEventListener('online', this.handleOnline);
+    window.removeEventListener('offline', this.handleOffline);
   }
 
-  updateOnlineStatus = () => {
-    this.setState({
-      isOnline: navigator.onLine,
-    });
+  handleOffline = () => {
+    this.setState({ status: 'offline' });
+  };
+
+  handleOnline = () => {
+    this.setState({ status: 'online' });
   };
 
   render() {
-    return (
-      <div className={this.state.isOnline ? 'status' : 'status status_offline'}>
-        {this.state.isOnline ? 'online' : 'оffline'}
-      </div>
-    );
+    const { status } = this.state;
+    return <div className={`status${status === 'offline' ? ' status_offline' : ''}`}>{status}</div>;
   }
 }
-export default ConnectionStatus;
 
-  
+export default Status;  
 
 
 
