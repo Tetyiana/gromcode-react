f43fc61df21d41d8ae5bcf8e9718071babc16423
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-12-10 17:41:12 +0200

    try18

--- forms/users-list-whith-filter/src/Filter.jsx
+++ forms/users-list-whith-filter/src/Filter.jsx
@@ -1,58 +1,17 @@
-// import React, { Component } from "react";
-
-
-// class Filter extends Component {
-//   render() {
-//     const { users } = this.props;
-//     const { filterText } = this.state;
-
-//     const usersWithFilter = users.filter(({ name }) => {
-//       if (filterText) {
-//         return name.toLowerCase().includes(filterText);
-//       }
-//       return (
-//         <Filter
-//           count={usersWithFilter.length}
-//           onChange={this.handleFilterChange}
-//           filterText={filterText}
-//         />
-//       )
-//     });
-//   }
-// }
-
-// export default Filter;
 import React, { Component } from "react";
 
 class Filter extends Component {
-  state = {
-    filterText: "",
-  };
-
-  handleFilterChange = (event) => {
-    const { onChange } = this.props;
-    const filterText = event.target.value.toLowerCase();
-
-    this.setState({
-      filterText,
-    });
-
- 
-      this.props.onChange(filterText);
-    
-  };
-
   render() {
-    const { count, filterText } = this.props;
+    const { filterText, count, onChange } = this.props;
 
     return (
       <div className="filter">
         <span className="filter__count">{count}</span>
-        <input
+          <input
           type="text"
           className="filter__input"
           value={filterText}
-          onChange={this.handleFilterChange}
+          onChange={onChange}
         />
       </div>
     );
@@ -60,6 +19,5 @@ class Filter extends Component {
 }
 
 export default Filter;
-
 
 
--- forms/users-list-whith-filter/src/UsersList.jsx
+++ forms/users-list-whith-filter/src/UsersList.jsx
@@ -1,42 +1,3 @@
-// import React, { Component } from "react";
-// import User from "./User.jsx"
-// import Filter from "./Filter";
-
-// class UsersList extends Component {
-//     state = {
-//     filterText: "",
-//   };
-
-//   handleFilterChange = (event) => {
-//     const { onChange } = this.props;
-//     const filterText = event.target.value.toLowerCase();
-
-//     this.setState({
-//       filterText,
-//     });
-
-//     };
-
-//   render() {
-//     const { users } = this.props;
-//     const userMap = users.map((user) => (
-//           <User key={user.id} name={user.name} age={user.age} />
-//         ))
-//     return (
-//      <div>
-//   <Filter />
-//   <ul class="users">
-//     <User />
-//   </ul>
-// </div>
-
-//     );
-//   }
-// }
-
-// export default UsersList;
-
-
 import React, { Component } from "react";
 import User from "./User.jsx";
 import Filter from "./Filter";
@@ -46,33 +7,33 @@ class UsersList extends Component {
     filterText: "",
   };
 
-  handleFilterChange = (filterText) => {
-    this.setState({
-      filterText,
-    });
+  handleFilterChange = (event) => {
+    // const { onChange } = this.props;
+    const filterText = event.target.value.toLowerCase();
+    this.setState({ filterText });
+  
   };
 
   render() {
     const { users } = this.props;
     const { filterText } = this.state;
 
-    const usersWithFilter = users.filter(({ name }) =>
+    const usersToDisplay = users.filter(({ name }) =>
       filterText ? name.toLowerCase().includes(filterText) : true
     );
 
-    const userMap = usersWithFilter.map((user) => (
-      <User key={user.id} name={user.name} age={user.age} />
-    ));
-
     return (
       <div>
-        {/* Передача count, filterText та onChange до компоненти Filter */}
         <Filter
-          count={usersWithFilter.length}
+          count={usersToDisplay.length}
           filterText={filterText}
           onChange={this.handleFilterChange}
         />
-        <ul className="users">{userMap}</ul>
+        <ul className="users">
+            {usersToDisplay.map((user) => (
+            <User key={user.id} name={user.name} age={user.age} />
+          ))}
+        </ul>
       </div>
     );
   }
