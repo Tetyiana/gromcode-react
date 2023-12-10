c8854736111a5d7b1bb576afd772a0131d8f4cbe
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-12-10 15:14:53 +0200

    екн2

--- forms/users-list-whith-filter/src/Filter.jsx
+++ forms/users-list-whith-filter/src/Filter.jsx
@@ -1,12 +1,54 @@
+// import React, { Component } from "react";
+// import UsersList from './UsersList.jsx';
+
+// class Filter extends Component {
+//   state = {
+//     filterText: "", // Додайте початковий стан для тексту фільтру
+//   };
+
+//   handleFilterChange = event => {
+//     this.setState({
+//       filterText: event.target.value.toLowerCase(),
+//     });
+//   };
+
+//   render() {
+//     const { users } = this.props;
+//     const { filterText } = this.state;
+
+//     const usersWithFilter = users.filter(({ name }) =>
+//       filterText && name.toLowerCase().includes(filterText.toLowerCase())
+//     );
+
+//     return (
+//       <div>
+//         <div className="filter">
+//           <span className="filter__count">{usersWithFilter.length}</span>
+//           <input
+//             type="text"
+//             className="filter__input"
+//             value={filterText}
+//             onChange={this.handleFilterChange}
+//           />
+//         </div>
+//         <UsersList users={usersWithFilter} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default Filter;
+
+
 import React, { Component } from "react";
-import UsersList from './UsersList.jsx';
+import UsersList from "./UsersList.jsx";
 
 class Filter extends Component {
   state = {
-    filterText: "", // Додайте початковий стан для тексту фільтру
+    filterText: "", 
   };
 
-  handleFilterChange = event => {
+  handleFilterChange = (event) => {
     this.setState({
       filterText: event.target.value.toLowerCase(),
     });
@@ -38,7 +80,5 @@ class Filter extends Component {
 }
 
 export default Filter;
-
-
 
 
