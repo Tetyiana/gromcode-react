4c30b25a2a99ff05378e0d8e42ebe4862dd1e609
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-12-12 22:49:33 +0200

    try1

---
+++ lifting-state-up/products-table/src/FilterableProductTable.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useState } from 'react';
+import  SearchBar  from "./SearchBar.jsx";
+import ProductTable from "./ProductTable.jsx";
+
+
+const FilterableProductTable=({ products })=> {
+  const [filterText, setFilterText] = useState('');
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  return (
+    <div>
+      <SearchBar 
+        filterText={filterText} 
+        inStockOnly={inStockOnly} 
+        onFilterTextChange={setFilterText} 
+        onInStockOnlyChange={setInStockOnly} />
+      <ProductTable 
+        products={products} 
+        filterText={filterText}
+        inStockOnly={inStockOnly} />
+    </div>
+  );
+}
+
+export default FilterableProductTable;
+
+
