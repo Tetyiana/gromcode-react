a2645b914be043af55cb74096d4a01f24bd10cce
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-12-12 17:58:34 +0200

    try2

--- lifting-state-up/products-list/src/ProductsList.jsx
+++ lifting-state-up/products-list/src/ProductsList.jsx
@@ -4,15 +4,18 @@ import React, { Component } from "react";
 class ProductsList extends Component {
 
   render() {
-    const total = this.props.cartItems.reduce((acc, item) => acc + item.price, 0,)
+    const total = this.props.cartItems.reduce((acc, item) => acc + item.price, 0,);
       return (
-      <div className="products">
-          {this.props.cartItems.map(({ id, name, price }) => (
+        <div className="products">
+          <ul class="products__list">
+            {this.props.cartItems.map(({ id, name, price }) => (
             <li key={id} className="products__list-item">
               <span className="products__item-name">{name}</span>
               <span className="products__item-price">{price}</span>
             </li>
           ))}
+          </ul>
+          
         <div className="products__total">{`Total: $${total}`}</div>
       </div>        
  )
