a39c934596c777246e331723c9b0a4a210501165
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-12-12 20:31:03 +0200

    try10

--- lifting-state-up/products-list/src/App.jsx
+++ lifting-state-up/products-list/src/App.jsx
@@ -25,7 +25,7 @@ class Page extends Component {
     <div className="page">
       <h1 className="title">{`Hello, ${this.state.userData.firstName} ${this.state.userData.lastName}`}</h1>
       <main className="content">
-        <ShoppingCart userName={this.state.userData}/>
+        <ShoppingCart userData={this.state.userData}/>
          <Profile
             userData={this.state.userData} 
             handleChange={this.handleChange} 
