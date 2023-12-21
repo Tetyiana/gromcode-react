a987d02a83b89e1863fa2821afd7a80470844fb5
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-12-21 19:30:52 +0200

    try4

--- react-router/react-router-task2/src/Product.jsx
+++ react-router/react-router-task2/src/Product.jsx
@@ -4,7 +4,9 @@ import React from "react";
 function Product({ match }) {
   // console.log(rest);
   return (
-   <div className="product">{`Product is a ${match.params.productId}`}</div>
+    <div className="product">
+      {`Product is a ${match.params.productId}`}
+    </div>
   )
 }
 export default Product;
